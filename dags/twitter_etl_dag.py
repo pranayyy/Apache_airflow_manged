f50ff79cbffad97c47fddfd16577b8eca204604a
pranayyy <pranaireddy007@gmail.com>
2025-06-24 14:02:57 +0530

    DAGS TF automated

--- dags/twitter_etl_dag.py
+++ dags/twitter_etl_dag.py
@@ -15,6 +15,7 @@ OUTPUT_BUCKET = "twitter-etl-output-bucket-unique-12345"
 
 def extract_tweets(**context):
     """Extract tweets from Twitter API"""
+    print("Extracting tweets...")
     BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAB562gEAAAAAIpezF4%2B%2FC%2F5hFmb9fYPEbYp7Alw%3DWCCNog6vecLlSV4CAlMZuD08DPGFu14fF4Qt4Z3HCIe33pZWqI"
     headers = {"Authorization": f"Bearer {BEARER_TOKEN}"}
     url = "https://api.twitter.com/2/tweets/search/recent"
